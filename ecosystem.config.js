7f1e4d394904203d49f2f2f2b8fb66e2615890d5
Anze Jensterle <anze@anze.xyz>
2019-11-23 08:32:04 +0100

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   apps : [{
-    name: 'API',
-    script: 'app.js',
+    name: 'prmon',
+    script: 'index.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
     instances: 1,
     autorestart: true,
     watch: false,

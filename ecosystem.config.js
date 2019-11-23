2a0c2fbb6caab783887005a999c02b451c06f443
Anze Jensterle <anze@anze.xyz>
2019-11-23 08:28:28 +0100

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 3000,
+      TZ: 'Europe/Ljubljana'
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3012,
+      TZ: 'Europe/Ljubljana'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'anze',
+      host : 'host.anze.xyz',
+      ref  : 'origin/master',
+      repo : 'prmon:fri/prmon.git',
+      path : '/opt/prmon',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
